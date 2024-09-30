2cb788b7639c184b5d80c744fd4290ea79375c5f
TDTUVU <tuvu100903@gmail.com>
2024-09-30 23:18:46 +0700

    card

---
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cardbirthday
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.happybirthday.R
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
